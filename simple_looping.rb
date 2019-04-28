31e81c84e264e90936f313dab77f6da148079d19
Blake Rose <bdecktor@gmail.com>
2019-04-28 20:28:18 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -19,8 +19,8 @@ end
 
 
 def times_iterator(number_of_times)
-  7.times do 
-    puts " Welcome to Flatiron School's Web Development Course!" 
+  7.times do
+    puts " Welcome to Flatiron School's Web Development Course!"
   end 
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
